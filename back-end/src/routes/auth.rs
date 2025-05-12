2b6c59a5775999ae1bd03333cbcf381bca88c03e
sektor7k <omeraydin2112@gmail.com>
2025-05-12 22:02:31 +0300

    signup routes

---
+++ back-end/src/routes/auth.rs
@@ -0,0 +1,11 @@
+use axum::{routing::post, Router};
+use sqlx::MySqlPool;
+use crate::handlers::auth_handlers::{signup};
+pub fn auth_routes(pool:MySqlPool) -> Router{
+    Router::new()
+
+    .route("/signup", post(signup))
+    // .route("/login", post(login))
+    .with_state(pool)
+
+}
