78119d1a4049f044c5dbb6ab9eee0a745f415573
sektor7k <omeraydin2112@gmail.com>
2025-05-14 02:30:25 +0300

    other server file update

--- back-end/src/models/user.rs
+++ back-end/src/models/user.rs
@@ -1,9 +1,8 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, sqlx::FromRow)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct User {
     pub id: String,
     pub username: String,
     pub email: String,
-    pub password: String,
 }
