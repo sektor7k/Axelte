2de15122b54dcda0d7170aa9e209f4fee883bb79
sektor7k <omeraydin2112@gmail.com>
2025-05-16 23:40:50 +0300

    workspace api handler

--- back-end/src/handlers/body_handlers.rs
+++ back-end/src/handlers/body_handlers.rs
@@ -2,6 +2,7 @@ use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
 use serde::{Serialize, Deserialize};
 use serde_json::json;
 use sqlx::MySqlPool;
+use uuid::Uuid;
 
 use crate::models::user::User;
 
@@ -21,13 +22,26 @@ pub async fn create_workspace(
     Json(payload): Json<CreateWorkspacePayload>,
 ) -> impl IntoResponse {
 
-    println!("user");
+    let ws_id = Uuid::new_v4().to_string();
 
-    println!("payload: {:?}", payload);
+    let result = sqlx::query!(
+        "INSERT INTO workspaces (id,name, description, owner_id) VALUES (?, ?, ?, ?)",
+        ws_id,
+        payload.name,
+        payload.description,
+        user.id
+    )
+    .execute(&pool)
+    .await
+    .map_err(|_| {
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(json!({ "message": "Database error" })))
+    });
+
+    //TODO: send email to all emails
     
     let body = Json(json!({
-        "message": "logged out successfully",
-        "data": user.id
+        "message": "workspace created successfully",
+        "workspaceId": ws_id
     }));
    
     (StatusCode::OK, body)
