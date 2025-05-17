785e304e72fdc078d56c1e4b101d83381b20e8c3
sektor7k <omeraydin2112@gmail.com>
2025-05-17 23:19:31 +0300

    get content page route

--- back-end/src/routes/body.rs
+++ back-end/src/routes/body.rs
@@ -2,7 +2,7 @@ use axum::routing::get;
 use axum::{middleware::from_fn_with_state, routing::post, Router};
 use sqlx::MySqlPool;
 use crate::middleware::auth_middleware::auth_middleware;
-use crate::handlers::body_handlers::{create_page, create_workspace, get_workspace_id, get_workspace_pages, get_workspaces};
+use crate::handlers::body_handlers::{create_page, create_workspace, get_page, get_workspace_id, get_workspace_pages, get_workspaces};
 
 pub fn body_routes(pool:MySqlPool) -> Router{
     Router::new()
@@ -11,5 +11,6 @@ pub fn body_routes(pool:MySqlPool) -> Router{
     .route("/workspaces/{workspaceId}", get(get_workspace_id))
     .route("/workspaces/{workspaceId}/pages", get(get_workspace_pages))
     .route("/create-page", post(create_page))
+    .route("/get-page/{pageId}", get(get_page))
     .layer(from_fn_with_state(pool.clone(), auth_middleware))
 }
