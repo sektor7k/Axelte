c84276d771313c183529d0cb048d2203bf93126d
sektor7k <omeraydin2112@gmail.com>
2025-05-17 19:44:45 +0300

    add api route

--- back-end/src/routes/body.rs
+++ back-end/src/routes/body.rs
@@ -1,10 +1,15 @@
+use axum::routing::get;
 use axum::{middleware::from_fn_with_state, routing::post, Router};
 use sqlx::MySqlPool;
 use crate::middleware::auth_middleware::auth_middleware;
-use crate::handlers::body_handlers::create_workspace;
+use crate::handlers::body_handlers::{create_page, create_workspace, get_workspace_id, get_workspace_pages, get_workspaces};
 
 pub fn body_routes(pool:MySqlPool) -> Router{
     Router::new()
     .route("/createworkspace", post(create_workspace))
+    .route("/get-workspaces", get(get_workspaces))
+    .route("/workspaces/{workspaceId}", get(get_workspace_id))
+    .route("/workspaces/{workspaceId}/pages", get(get_workspace_pages))
+    .route("/create-page", post(create_page))
     .layer(from_fn_with_state(pool.clone(), auth_middleware))
 }
